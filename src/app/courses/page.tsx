98f63ba48f5d91e3f7d3e5439df35ef2ad01e0bd
Pratham Choudhary <prathamchoudhary924@gmail.com>
2024-05-22 22:54:34 +0530

    first commit

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import Link from "next/link";
+import coursesData from "@/data/music_courses.json";
+
+
+function page() {
+  return (
+    <div >
+        Test 1
+    </div>
+  )
+}
+
+export default page
